725f0f768a907076fd4f152d75aab7887dcdb7dd
Travis DIxon <the.trav@gmail.com>
2023-10-04 11:27:37 +1100

    fix timezone, handle multiple calendars, decompose functions

--- README.md
+++ README.md
@@ -7,8 +7,48 @@ Playing around with my new waveshare 7in3g e-paper screen, going to try and rend
 There's something a bit screwy around "burn in" I've read both that it occurs and also that it doesn't occur.  Try to avoid leaving the screen with stuff on it for more than a day, if you want it always on, do the clear thing at least daily so it jiggles all those pixels around.
 
 TODO: add venv instructions
-TODO: add dependencies to requirements.txt
+# venv
+Keep the python peas from touching the other python carrots
+
+create a venv using:
+```
+python -m venv .venv
+```
+
+## windows
+put powershell into venv mode using:
+```
+.\venv\Scripts\activate.ps1
+```
+
+## linux
+put powershell into venv mode using:
+```
+source ./venv/Scripts/activate
+```
+
+# Dependencies
+
+
+## system
+The thing requires a few system dependencies.  
+This is really only relevant on the pi itself, which is running a debian thingy, so apt-get with:
+```
 TODO: add system dependencies
+```
+
+## pip
+python dependencies are managed in `requirements.txt`
+when in venv mode install using:
+```
+pip install -r requirements.txt
+```
+TODO: add waveshare dependencies to requirements.txt
+
+# Runtime
 
 TODO: add runtime instructions
+## Google Authentication
+
+
 
---
+++ google_calendar.py
@@ -0,0 +1,116 @@
+from __future__ import print_function
+
+import datetime
+from zoneinfo import ZoneInfo
+import os.path
+from dataclasses import dataclass, field
+from operator import attrgetter
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+TIMEZONE = "Australia/Melbourne"
+
+@dataclass
+class GoogleCalendar:
+    id: str
+    name: str
+
+@dataclass
+class Event:
+    owner: str
+    summary: str
+    start_time: datetime.time = None
+    end_time: datetime.time = None
+
+@dataclass
+class CalendarDay:
+    date: datetime.date
+    whole_day_events: list[Event] = field(default_factory=list)
+    timed_events: list[Event] = field(default_factory=list)
+
+def load_google_creds():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    token_filename = "token.json"
+    if os.path.exists(token_filename):
+        creds = Credentials.from_authorized_user_file(token_filename, SCOPES)
+
+    if creds and creds.expired and creds.refresh_token:
+        try:
+            creds.refresh(Request())
+        except:
+            creds = None
+
+    if not creds or not creds.valid:
+        # TODO: the device has no input, so we need to replace this with some other flow
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", SCOPES)
+        creds = flow.run_local_server(port=0)
+        with open(token_filename, 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+def list_google_calendars(creds):
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+        result = service.calendarList().list().execute()
+        return [GoogleCalendar(c["id"], c["summary"]) for c in result.get("items", [])]
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return []
+    
+def list_google_events(creds, calendar_id, min, max):
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+        # TODO: fix handling of timezone, we don't actually want UTC, we want local time
+        events_result = service.events().list(calendarId=calendar_id, timeMin=min.isoformat(),
+                                              timeMax=max.isoformat(), singleEvents=True,
+                                              orderBy='startTime').execute()
+        return events_result.get('items', [])
+    
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+        return []
+
+def add_events_to_calendars(events_from_google, calendar_name, calendars):
+        for event in events_from_google:
+            start = event['start']
+            start_date = start.get('date', start.get("dateTime"))
+            date = datetime.datetime.fromisoformat(start_date).date()
+            dest = next((c for c in calendars if c.date == date), None)
+            if dest:
+                event = Event(
+                    owner = calendar_name,
+                    summary = event['summary']
+                )
+                if 'dateTime' in start:
+                    event.start_time = datetime.datetime.fromisoformat(start['dateTime'])
+                    dest.timed_events.append(event)
+                else:
+                    dest.whole_day_events.append(event)
+
+def get_calendars():
+    creds = load_google_creds()
+    google_calendars = list_google_calendars(creds)
+    today = datetime.datetime.combine(datetime.date.today(), datetime.time.min,  tzinfo=ZoneInfo(TIMEZONE))
+    tomorrow = (today + datetime.timedelta(days=1))
+    calendars = [CalendarDay(date=today.date()), CalendarDay(date=tomorrow.date())]
+    for gcal in google_calendars:
+        events = list_google_events(creds, gcal.id, today, tomorrow)
+        add_events_to_calendars(events, gcal.name, calendars)
+    for cal in calendars:
+        cal.whole_day_events.sort(key=attrgetter("start_time"))
+        cal.timed_events.sort(key=attrgetter("start_time"))
+    return calendars
+
+if __name__ == '__main__':
+   print(f"calendars: {get_calendars()}")
