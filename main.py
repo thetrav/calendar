814204fec3318f098102516c1584227de6baed0f
Travis DIxon <the.trav@gmail.com>
2023-11-02 15:58:32 +1100

    test on hardware

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ from google_calendar import get_calendars, test_data
 from rendering import layout_calendars
 from model import Surface
 
-# from waveshare_epd import epd7in3g
+from waveshare_epd import epd7in3g
 
 calendars = get_calendars()
 # calendars = test_data()
 
 image = layout_calendars(calendars, Surface())
-image.show("test")
+# image.show("test")
 
-# try:
-#     epd = epd7in3g.EPD()
-#     epd.init()
-#     epd.display(epd.getbuffer(image))
-#     epd.sleep()
+try:
+    epd = epd7in3g.EPD()
+    epd.init()
+    epd.display(epd.getbuffer(image))
+    epd.sleep()
 
-# except IOError as e:
-#     print(f"error: {e}")
+except IOError as e:
+    print(f"error: {e}")
 
-# except KeyboardInterrupt:
-#     print("ctrl + c:")
-#     epd7in3g.epdconfig.module_exit()
-#     exit()
+except KeyboardInterrupt:
+    print("ctrl + c:")
+    epd7in3g.epdconfig.module_exit()
+    exit()
